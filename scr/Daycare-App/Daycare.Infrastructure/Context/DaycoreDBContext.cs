73029dd5a8cbc0a42fe28e210a778b36c54324cb
sarahhxxx <sarahfiguereo7@gmail.com>
2025-11-18 13:47:29 -0400

    Remove

---
+++ scr/Daycare-App/Daycare.Infrastructure/Context/DaycoreDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Daycare.Domain.Core;
+using Daycare.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Daycare.Infrastructure.Context
+{
+    public class DaycareDBContext : DbContext
+    {
+        public DaycareDBContext(DbContextOptions<DaycareDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Child> Children { get; set; }
+        public DbSet<Guardian> Guardians { get; set; }
+        public DbSet<Activity> Activities { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+
+    }
+
+}
